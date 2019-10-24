e9315e9f994151354bb1df674e39170f03d7111f
github username <github email address>
2019-10-24 15:58:16 -0700

    Polishing up

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,7 +1,9 @@
 class SkillsController < ApplicationController
   def index
+    @skills = Skill.all
   end
 
   def show
+    @skill = Skill.find(params[:id])
   end
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,6 +1,6 @@
 class Character < ApplicationRecord
     #validates :name,:equipment,:int,:str,:dex,:wis,:cha,:con,:hp,:ac,:alignment,:size,:languages,:saving_throws,:initiative,:job_id, :race_id, :skills_id, :spells_id, :skills_id, presence: true
-
+    validates :int, :str, :dex, :cha, :wis, :con, numericality:{less_than_or_equal_to: 8}
     belongs_to :user
     belongs_to :race
     belongs_to :job
